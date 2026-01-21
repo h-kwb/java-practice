103405f5d045f08c7c84909fa18c0f1d18900c1d
h-kwb <k.hiroto.6302@gmail.com>
2026-01-21 13:04:39 +0900

    Add Person class with constructor and introduce method

--- oop/Person.java
+++ oop/Person.java
@@ -1,1 +1,13 @@
+public class Person {
+  private String name;
+  private int age;
 
+  public Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public void introduce() {
+    System.out.println("My name is " + name + " and I'm " + age + " years old.");
+  }
+}
