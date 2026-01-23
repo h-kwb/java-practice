081589f11e3e2879d0ccab10064508562e5476ff
h-kwb <k.hiroto.6302@gmail.com>
2026-01-23 13:16:23 +0900

    IfCheck.java を作成

---
+++ basics/IfCheck.java
@@ -0,0 +1,11 @@
+public class IfCheck {
+    public static void main(String[] args) {
+        String message = "birthday";
+        
+        if (message.equals("birthday")) {
+            System.out.println("Happy birthday!");
+        } else {
+            System.out.println("Have a nice day!");
+        }
+    }
+}
