9babde08b2fa1397ff90db4967725a1e1c3a0962
h-kwb <k.hiroto.6302@gmail.com>
2026-01-24 01:49:19 +0900

    Create Pyramid.java

---
+++ basics/Pyramid.java
@@ -0,0 +1,8 @@
+public class Pyramid {
+  public static void main(String[] args) {
+    String boxs = "■";
+    for (int i = 0; i < 10; i++) {
+      System.out.println(box.repeat(i));
+    }
+  }
+}
