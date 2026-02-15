fa66bcb8b83a600ea1a6aab8758effeb42cdb9cc
h-kwb <k.hiroto.6302@gmail.com>
2026-02-15 21:46:46 +0900

    Update IfCheck.java

--- basics/IfCheck.java
+++ basics/IfCheck.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class IfCheck {
     public static void main(String[] args) {
-        String message = "birthday";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Is today your birthday?");
+        String answer = sc.nextLine();
         
-        if (message.equals("birthday")) {
+        if (answer.equalsIgnoreCase("birthday")) {
             System.out.println("Happy birthday!");
         } else {
             System.out.println("Have a nice day!");
         }
+        sc.close();
     }
 }
